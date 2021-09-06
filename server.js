b0d4ddcc846f14aa7f191526ad07e50ff528a776
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-09-05 22:08:08 -0600

    updated server

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workoutTrackerDB',
@@ -12,10 +24,5 @@ mongoose.connect(
       useFindAndModify: false
     }
 );
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`)); 
